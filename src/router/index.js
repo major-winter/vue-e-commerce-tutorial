17b84e41b4ea77b94c7fab9b3fb286fbc719f4fe
Jason Chu <tuanhoangchu@outlook.com>
2020-11-21 11:06:27 +0700

    complete app

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+	{
+		path: '/',
+		component: () => import('../layouts/Base.vue'),
+		children: [
+			{ path: '', component: () => import('../home/Products.vue') },
+			{
+				path: 'details/:idProduct',
+				component: () => import('../home/Details.vue'),
+			},
+			{ path: 'cart', component: () => import('../home/Cart.vue') },
+		],
+	},
+	{
+		path: '/login',
+		component: () => import('../account/Login.vue'),
+	},
+];
+
+const router = new VueRouter({
+	mode: 'history',
+	// base: process.env.BASE_URL,
+	routes,
+});
+
+export default router;
